11a1ace260dcf900becfc7822dc1ea9efb0bd412
emeraldtip <emeraldtipyt@gmail.com>
2024-04-11 15:20:34 +0300

    Started working on CLI implementation

---
+++ src/main/java/goldendelicios/lite2edit/Cli.java
@@ -0,0 +1,58 @@
+package goldendelicios.lite2edit;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cli {
+
+    private ArrayList<File> files = new ArrayList<File>();
+    //all possible arguments
+
+    public void cli(String[] args) {
+        System.out.println("----Starting Lite2Edit CLI----");
+        if (args.length>1) {
+            for (String s: args) {
+                if (!s.equals("--cli")) {
+                    File file = new File(s);
+                    if (file.exists()) {
+                        files.add(file);
+                    }
+                }
+            }
+
+            StringBuilder s = new StringBuilder();
+
+
+            for (int i = 0; i<files.size(); i++) {
+                String working = "Working... (" + i + "/" + files.size() + " complete)";
+
+                long start = System.currentTimeMillis();
+                File input = files.get(i);
+                try {
+                    File parent = input.getParentFile();
+                    Lite2Edit.dir = parent;
+                    List<File> outputs = Converter.litematicToWorldEdit(input, parent);
+
+                    if (outputs.isEmpty()) {
+                        s.append(input.getName() + " is not a valid litematic file\n");
+                    }
+                    else {
+                        for (File output : outputs) {
+                            s.append("Exported to " + output.getName() + "\n");
+                        }
+                    }
+                    long time = System.currentTimeMillis() - start;
+                    System.out.println("Conversion took " + time + "ms");
+                } catch (Throwable e) {
+                    s.append("Error while converting " + input.getName() + ":\n" + e + "\n");
+                    Lite2Edit.handleException(e);
+                }
+            }
+        }
+        else {
+            System.out.println("No valid filepaths found!");
+            System.out.println("Correct usage: java -jar Lite2Edit-x.x.x.jar --cli [Path to file 1] [Path to file 2]...");
+        }
+    }
+}
--- src/main/java/goldendelicios/lite2edit/Lite2Edit.java
+++ src/main/java/goldendelicios/lite2edit/Lite2Edit.java
@@ -22,7 +22,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileFilter;
 
 public class Lite2Edit {
-	private static File dir = new File(System.getProperty("user.dir"));
+	public static File dir = new File(System.getProperty("user.dir"));
 	private static PrintStream errorFile;
 	private static ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(1);
 
@@ -32,26 +32,36 @@ public class Lite2Edit {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		if (args.length>0) {
+			if (args[0].equals("--cli")) {
+				new Cli().cli(args);
+				return;
+			}
+		}
+		init();
+	}
+
+	private static void init()
+	{
 		JFrame frame = new JFrame("Lite2Edit");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 200);
 		frame.setLocationRelativeTo(null);
-		
+
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Pick 1 or more Litematic files");
-		
+
 		JTextArea textArea = new JTextArea(6, 0);
 		textArea.setEditable(false);
 		JScrollPane scrollPane = new JScrollPane(textArea);
 		scrollPane.setBorder(new EmptyBorder(10, 10, 10, 10));
-		
+
 		JButton browse = new JButton("Browse");
 		browse.addActionListener(getBrowseListener(textArea, browse));
-		
+
 		panel.add(label);
 		panel.add(browse);
-		
+
 		frame.getContentPane().add(BorderLayout.CENTER, panel);
 		frame.getContentPane().add(BorderLayout.SOUTH, scrollPane);
 		frame.setVisible(true);
@@ -104,7 +114,7 @@ public class Lite2Edit {
 		};
 	}
 	
-	private static void handleException(Throwable e) {
+	public static void handleException(Throwable e) {
 		e.printStackTrace();
 		if (errorFile == null) {
 			try {
